2c9150e18c89a43dbd5042053992ea45ef83c7ed
Corcso <115189987+Corcso@users.noreply.github.com>
2025-01-25 18:34:40 +0000

    Added; Recentre

--- Player.cs
+++ Player.cs
@@ -7,6 +7,8 @@ public partial class Player : XROrigin3D
 
     public override void _Ready()
     {
+
+        // Open XR Setup, from Godot documentation. 
         _xrInterface = XRServer.FindInterface("OpenXR");
         if (_xrInterface != null && _xrInterface.IsInitialized())
         {
@@ -22,10 +24,23 @@ public partial class Player : XROrigin3D
         {
             GD.Print("OpenXR not initialized, please check if your headset is connected");
         }
+
+
+        // Player setup
+
+        // Pressing Y (Left Controler) will trigger a recentre. Moving head to 0, 1.8 , 0 by adjusting XR origin. 
+        GetNode<XRController3D>("./Left Hand").ButtonPressed += (string name) => {
+            if (name == "by_button") Recentre();
+        };
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
 	{
 	}
+
+    private void Recentre() {
+        // Recenter with an assumed height of 1.8 meters. 
+        GlobalPosition = new Vector3(0, 1.8f, 0) - GetNode<Node3D>("./Head").GlobalPosition;
+    }
 }
